ff63aaec7df0c7882aff4d1cb862a01b4cdc781a
indo <indogusmasarungsamudra@gmail.com>
2019-03-02 22:10:43 +0700

    Initial Commit

---
+++ JsoParsing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsoParsing
+//
+//  Created by indo gusmas arung samudra on 02/03/19.
+//  Copyright © 2019 indo gusmas arung samudra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
